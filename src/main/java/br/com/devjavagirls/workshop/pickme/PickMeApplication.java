152f405fea1b52738f08ea39f2da34ca38cc3669
myakiju <j.myaki@gmail.com>
2018-12-08 15:15:51 -0200

    commit inicial

---
+++ src/main/java/br/com/devjavagirls/workshop/pickme/PickMeApplication.java
@@ -0,0 +1,12 @@
+package br.com.devjavagirls.workshop.pickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickMeApplication.class, args);
+	}
+}
